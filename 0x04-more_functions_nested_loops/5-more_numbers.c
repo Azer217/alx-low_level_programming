b0f683bd6856c8d1ccf8e8b5d900b4194ed4e3bc
Azer217 <adnane.skylove@gmail.com>
2023-03-23 10:01:53 -0700

    e

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- * more_numbers - print more numbers
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  */
 
 void more_numbers(void)
-{
-	void more_numbers(void)
 {
 	int l;
 	char i;
