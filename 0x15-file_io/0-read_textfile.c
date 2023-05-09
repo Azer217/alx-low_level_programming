904945fde2d3ad8fef29835c0c24783d5928ec01
Azer217 <adnane.skylove@gmail.com>
2023-05-09 13:04:50 -0700

    jlga

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,25 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
- *
- * Return: numbers of letters printed. It fails, returns 0.
+ * read_textfile - reads a text file and prints it
+ *			to the POSIX standard output
+ * @letters: number of letters it should read and print
+ * @filename: text file
+ * Return: actual number of letters it could read and print
+ *		if the file can not be opened or read, return 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
+	ssize_t t, R, W;
 	char *buf;
 
-	if (!filename)
+	R = open(filename, O_RDONLY);
+	if (R == -1)
 		return (0);
-
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
-	close(fd);
-
+	buf = malloc(sizeof(char) * letters);
+	t = read(R, buf, letters);
+	W = write(STDOUT_FILENO, buf, t);
 	free(buf);
-
-	return (nwr);
+	close(R);
+	return (W);
 }
